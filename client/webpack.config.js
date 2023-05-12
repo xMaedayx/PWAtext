3259c6f0915841923f1e1997f4f04e69a1cea8fd
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-05-11 18:10:24 -0700

    stash

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'J.A.T.E.'
       }),
      
       // Injects our custom service worker
@@ -40,9 +40,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'J.A.T.E.',
+        short_name: 'Text-editor',
+        description: 'Fast Notes',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
