6bc45022a26cc4e2dd6605d0b97108cd11bbf048
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-05-11 18:23:31 -0700

    update

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,11 +3,11 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 //  Add an event handler to the `beforeinstallprompt` event
-// window.addEventListener('beforeinstallprompt', (event) => {
-//     window.deferredPrompt = event;
-//     // Remove the hidden class from the button.
-//     butInstall.classList.toggle('hidden', false);
-// });
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+});
 
 // Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
